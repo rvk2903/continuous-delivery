1b2d8a4722866ec88a24ac366ce45088fb8e86f1
rvk2903 <103313168+rvk2903@users.noreply.github.com>
2022-10-12 13:22:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('my name is vinod'));
+app.get('/', (req, res) => res.send('this code was changes'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
